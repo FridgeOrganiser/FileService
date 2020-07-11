15ace400c975dcc836bfd1db694b122fa54ceacb
Stanislav Hrytsyshyn <grucushun90@gmail.com>
2020-07-11 17:47:49 +0300

    Add logic of adding image entity to database

--- service/resources/dummy_resource.py
+++ service/resources/dummy_resource.py
@@ -2,12 +2,17 @@
 from flask import jsonify
 from flask_restful import Resource
 from flask_api import status
+from flask.wrappers import Response
 from service import api
 
 
 class DummyResource(Resource):
     """Basic resource for testing if service is up and running."""
     def get(self):
+        """GET method of resource
+            Returns:
+                Response: response object with related status
+        """
         return jsonify({"Message:": "Hello World"},
                        status.HTTP_200_OK)
 
--- service/resources/load_resource.py
+++ service/resources/load_resource.py
@@ -9,29 +9,40 @@ from service.utils.utils import (
     get_image_hash_value, is_input_file_correct,
     save_file_and_get_location
 )
+from service.services.postgres_service import PG_Query
 
 
 class FileLoadingResource(Resource):
+    """File Loading resource for managing image loading into system."""
 
     def post(self):
-
+        """POST method of resource for downloading user images to system.
+                Returns:
+                    Response: response object with related status
+                Raises:
+                    TypeError: in case of objects incorrect types
+        """
         if "input_img" in request.files:
             input_file = request.files["input_img"]
             file_name = input_file.filename
 
             if not is_input_file_correct(file_name):
                 raise TypeError("Unsupported File Type")
-
-            import pdb;pdb.set_trace()
             
             current_image = Image.open(input_file.stream)
             image_hash = get_image_hash_value(current_image)
             image_location = save_file_and_get_location(current_image, file_name)
             image_size = len(current_image.tobytes())
 
-            print(image_hash, image_location, image_size)
+            PG_Query.create_new_entity(image_hash=image_hash,
+                                       image_location=image_location,
+                                       image_size=image_size,
+                                       image_name=file_name)
 
-            return jsonify({}, status.HTTP_200_OK)
+            return jsonify(
+                {"message": "Created new image entity"},
+                status.HTTP_200_OK
+            )
 
 
 api.add_resource(FileLoadingResource, '/load')
